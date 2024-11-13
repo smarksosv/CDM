e47b45180eeb05b955b854ef5ec8d9ff1a816ca4
stevemarksosvfedex <steve.marks.osv@fedex.com>
2024-11-13 18:09:50 +0000

    Removed languages, cleaned docs folder

--- objectModel/TypeScript/Utilities/Network/CdmNumberOfRetriesExceededException.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
-
-export class CdmNumberOfRetriesExceededException extends Error {
-    constructor(errorMsg : string) {
-        super (errorMsg);
-        Object.setPrototypeOf(this, CdmNumberOfRetriesExceededException.prototype)
-    }
-}
--- objectModel/TypeScript/Utilities/Network/CdmTimedOutException.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
-
-export class CdmTimedOutException extends Error {
-    constructor(errorMsg : string) {
-        super (errorMsg);
-        Object.setPrototypeOf(this, CdmTimedOutException.prototype)
-    }
-}
